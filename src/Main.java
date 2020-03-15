76eb5e4c7932f33c84e9a4830e6c9df265d15645
Szymon <s.bukowski07@gmail.com>
2020-03-15 14:41:27 +0100

    main for testing

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,56 @@
+import algorithm.GreedyAlgorithm;
+import algorithm.RandomAlgorithm;
 import loader.Loader;
 import individual.Individual;
-
-import java.util.ArrayList;
+import problem.TSProblem;
 
 public class Main {
 
     public static void main(String args[]){
 
+        printHeader("Load file", 40, '#');
         Loader loader = new Loader();
 //        loader.loadByFileName("ali535.tsp");
 //        loader.loadByFileName("berlin11_modified.tsp");
-        loader.loadByFileName("berlin52.tsp");
+//        loader.loadByFileName("berlin52.tsp");
 //        loader.loadByFileName("fl417.tsp");
 //        loader.loadByFileName("kroA100.tsp");
-//        loader.loadByFileName("kroA150.tsp");
+        loader.loadByFileName("kroA150.tsp");
 //        loader.loadByFileName("kroA200.tsp");
 //        loader.loadByFileName("pr2392.tsp");
-        loader.print();
-        double[][] distance = loader.createDistanceMatrix();
 
-        ArrayList<Individual> individuals = new ArrayList<>();
-        for(int ii=0; ii<100; ii++){
-            individuals.add(new Individual(loader.DIMENSION));
-        }
-        for(Individual i: individuals){
-            System.out.println(i);
-            System.out.println(i.fittnes(distance));
-        }
+        TSProblem problem = loader.getProblem();
 
+        printHeader("Random alg", 40, '#');
+        RandomAlgorithm randomAlgorithm = new RandomAlgorithm(problem, 2000000);
+        Individual<Integer> randomSolution = randomAlgorithm.findSolution();
+        System.out.println("Random:" + problem.getFitnessCounter().evaluate(randomSolution));
 
+        printHeader("Simple greedy alg", 40, '#');
+        GreedyAlgorithm greedyAlgorithm = new GreedyAlgorithm(problem, 0);
+        Individual<Integer> greedySolution = greedyAlgorithm.findSolution();
+        System.out.println("Greedy:" + problem.getFitnessCounter().evaluate(greedySolution));
+
+        printHeader("Full greedy alg", 40, '#');
+        Individual<Integer> fullgreedySolution = greedyAlgorithm.fullSearch();
+        System.out.println("FullGreedy:" + problem.getFitnessCounter().evaluate(fullgreedySolution));
 
     }
+
+    public static void printHeader(String header, int width, char fillSign){
+        int rest = width - header.length();
+        StringBuilder res = new StringBuilder();
+
+        for(int ii=0; ii<rest; ii++){
+            res.append(fillSign);
+        }
+
+        res.append(' ').append(header).append(' ');
+
+        for(int ii=0; ii<rest; ii++){
+            res.append(fillSign);
+        }
+        System.out.println(res.toString());
+    }
+
 }
