dbdfb18a9947c7b1dfa7b2760c96f55b3334039f
Szymon <s.bukowski07@gmail.com>
2020-03-20 11:50:25 +0100

    SwapMutation implementation

--- src/geneticOperators/mutation/SwapMutation.java
+++ src/geneticOperators/mutation/SwapMutation.java
@@ -2,11 +2,38 @@ package geneticOperators.mutation;
 
 import individual.Individual;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class SwapMutation implements IMutation {
+
+    Random rd;
+
+    public SwapMutation(){
+        rd = new Random();
+    }
 
     @Override
     public Individual mutate(Individual individual) {
-        Individual res = new Individual();
-        return res;
+
+        ArrayList<Integer> genome = individual.getGenome();
+
+        int firstIndex = rd.nextInt(genome.size());
+        int secondIndex = rd.nextInt(genome.size());
+
+        while(firstIndex == secondIndex){
+            secondIndex = rd.nextInt(genome.size());
+        }
+        swap(genome, firstIndex, secondIndex);
+
+        return new Individual(genome);
+    }
+
+    private void swap(ArrayList<Integer> genome, int first, int second){
+        int first_val = genome.get(first);
+        int second_val = genome.get(second);
+
+        genome.set(first, second_val);
+        genome.set(second, first_val);
     }
 }
